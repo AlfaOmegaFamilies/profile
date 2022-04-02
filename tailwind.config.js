12d1c6b3c47e30da07f6a860264c4050665a475b
bagasta123 <bagasta@upnvj.ac.id>
2022-04-03 03:46:43 +0700

    Mengupload Church Profile

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding:'16px',
+    },
+    extend: {
+      colors:{
+        primary:'#323377',
+        secondary:'#64748b',
+        yt:'#FF0000',
+        ig:'#E4405F',
+        fb:'#1778F2',
+        wa:'#25D366',
+        gmail:'#EA4335',
+        dark:'#0f172a',
+      },
+      screens:{
+        '2xl':'1320px',
+      },
+    },
+  },
+  plugins: [],
+}
